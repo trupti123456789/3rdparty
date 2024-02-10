1657e4c400e9bb17fda41833329abf51a56aba45
Trupti  Shantaram Chaudhary <trupti.c@cloudfulcrum.com>
2024-02-10 13:17:01 +0530

    dd

--- Resources/my_tabula2.py
+++ Resources/my_tabula2.py
@@ -1,5 +1,7 @@
 # pdf_table_extractor.py
-import tabula
+from tabula import read_pdf
+from robot.api import logger
+
 
 def extract_pdf_tables(pdf_path):
     # This function uses tabula-py to extract tables from a PDF and returns them
