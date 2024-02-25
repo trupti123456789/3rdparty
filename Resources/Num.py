5061262786fb9520a6882bddd962d5458118f07c
Trupti  Shantaram Chaudhary <trupti.c@cloudfulcrum.com>
2024-02-25 12:00:41 +0530

    ww

--- Resources/Num.py
+++ Resources/Num.py
@@ -1,8 +1,11 @@
 from robot.api import logger
 
-class AdditionLibrary:
+class Num():
     def add_two_numbers(self, number1, number2):
         """Adds two numbers and logs the result."""
-        result = number1 + number2
+        # Convert the string arguments to integers
+        num1 = int(number1)
+        num2 = int(number2)
+        result = num1 + num2
         logger.info(f"The result is: {result}")
         return result
