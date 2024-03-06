08e439788e8a197af46ca00f98d19f2038518699
Trupti  Shantaram Chaudhary <trupti.c@cloudfulcrum.com>
2024-03-06 16:57:18 +0530

    ff

--- Resources/mongo.py
+++ Resources/mongo.py
@@ -2,6 +2,8 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from robot.api import logger
 
+
+
 class mongo():
     client = None
     db = None  # Add this line to store the database object
@@ -26,6 +28,39 @@ class mongo():
     def find_records(self, collection_name, key, value):
         collection = self.db[collection_name]
         query = {key: value}
+        documents = collection.find(query)
+        results = []
+        for doc in documents:
+            results.append(doc)
+            logger.info(str(doc), also_console=True)
+        return results
+
+    def insert_record(self, collection_name, data):
+        collection = self.db[collection_name]
+        result = collection.insert_one(data)
+        logger.info(f"Inserted record with id {result.inserted_id}", also_console=True)
+        return result.inserted_id
+
+    def update_records(self, collection_name, query, new_values):
+        collection = self.db[collection_name]
+        result = collection.update_many(query, {'$set': new_values})
+        logger.info(f"Updated {result.modified_count} records.", also_console=True)
+        return result.modified_count
+
+    def delete_records(self, collection_name, query):
+        collection = self.db[collection_name]
+        result = collection.delete_many(query)
+        logger.info(f"Deleted {result.deleted_count} records.", also_console=True)
+        return result.deleted_count    
+
+    def delete_records2(self, collection_name, query):
+        collection = self.db[collection_name]
+        result = collection.delete_many(query)
+        logger.info(f"Deleted {result.deleted_count} records.", also_console=True)
+        return result.deleted_count    
+
+    def Logical_oper_query(self, collection_name, query):
+        collection = self.db[collection_name]
         documents = collection.find(query)
         results = []
         for doc in documents:
